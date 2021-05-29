94577ed98c711df324794d95e828289de868f3e1
Paul Miller <paul@paulmillr.com>
2021-05-29 19:45:34 +0300

    Add ability to build single file for browsers

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
+export default {
+  input: 'index.js',
+  output: {
+    file: 'build/noble-bls12-381.js',
+    format: 'umd',
+    name: 'nobleBls12381',
+    exports: 'named',
+    preferConst: true,
+  },
+  plugins: [resolve(), commonjs()],
+};
