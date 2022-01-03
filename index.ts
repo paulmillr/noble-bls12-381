1844ed359534bcdceb44d7f17370b1a55e3ed939
Paul Miller <paul@paulmillr.com>
2022-01-04 01:47:19 +0200

    Use improved hex helpers from noble libs

--- index.ts
+++ index.ts
@@ -124,11 +124,12 @@ function bytesToNumberBE(bytes: Uint8Array) {
   return value;
 }
 
+const hexes = Array.from({ length: 256 }, (v, i) => i.toString(16).padStart(2, '0'));
 function bytesToHex(uint8a: Uint8Array): string {
   // pre-caching chars could speed this up 6x.
   let hex = '';
   for (let i = 0; i < uint8a.length; i++) {
-    hex += uint8a[i].toString(16).padStart(2, '0');
+    hex += hexes[uint8a[i]];
   }
   return hex;
 }
@@ -141,7 +142,11 @@ function hexToBytes(hex: string): Uint8Array {
   const array = new Uint8Array(hex.length / 2);
   for (let i = 0; i < array.length; i++) {
     const j = i * 2;
-    array[i] = Number.parseInt(hex.slice(j, j + 2), 16);
+    const hexByte = hex.slice(j, j + 2);
+    if (hexByte.length !== 2) throw new Error('Invalid byte sequence');
+    const byte = Number.parseInt(hexByte, 16);
+    if (Number.isNaN(byte)) throw new Error('Invalid byte sequence');
+    array[i] = byte;
   }
   return array;
 }
